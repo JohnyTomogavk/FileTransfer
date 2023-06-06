05a483801dc25979ae92669f9007700d799e1dcb
Johny Tomogavk <boss.vanya20@mail.ru>
2023-06-06 17:28:33 +0300

    - Implement base command class; - Create MainWindowViewModel; - Configure property changer notifier.

---
+++ FileTransfer/Command/AppCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FileTransfer.Command;
+
+internal class AppCommand : ICommand
+{
+    private Action execute;
+    private Func<bool> canExecute;
+
+    public event EventHandler CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public AppCommand(Action execute, Func<bool> canExecute = null)
+    {
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return this.canExecute == null || this.canExecute();
+    }
+
+    public void Execute(object parameter)
+    {
+        this.execute();
+    }
+}
